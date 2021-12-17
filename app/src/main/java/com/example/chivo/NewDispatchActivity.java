3878f2d6bf999005fc4fefa36eb4a2fbfd4290bc
Nikoloman <hectorvel52@gmail.com>
2021-12-17 17:11:28 -0600

    Cambio de interfaz de "Nuevo despacho", cambio en EditText por Spinner para evitar errores de letras a la hora de registrar nuevos despachos

--- app/src/main/java/com/example/chivo/NewDispatchActivity.java
+++ app/src/main/java/com/example/chivo/NewDispatchActivity.java
@@ -8,8 +8,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -23,8 +25,9 @@ import java.util.Map;
 public class NewDispatchActivity extends AppCompatActivity {
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-    EditText txt_sucursal, txt_folio, txt_fecha, txt_hora, txt_ruta, txt_dueño, txt_placas, txt_unidad, txt_litros;
+    EditText txt_folio, txt_fecha, txt_hora, txt_placas, txt_litros;
     CheckBox checkStatus;
+    Spinner txt_sucursal, txt_ruta, txt_dueño, txt_unidad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,19 +45,35 @@ public class NewDispatchActivity extends AppCompatActivity {
         txt_litros = findViewById(R.id.edit_Litros);
 
         checkStatus = findViewById(R.id.checkBox_Status);
+
+        String [] Sucursales = {"Sucursal", "Vicente Guerrero", "Lopez Mateos"};
+        String [] Rutas = {"Ruta", "632", "633", "Express", "629"};
+        String [] Dueños = {"Dueño", "Tony", "Goyo", "Jaime Castro", "Fantasma", "Beto Paez", "Palma", "Paulina", "David"};
+        String [] Unidades = {"Unidad","001", "002", "005", "006", "007", "008", "009", "010", "011", "012", "013"};
+
+
+        ArrayAdapter <String> sucursal = new ArrayAdapter<String>(this, R.layout.spinner_item_diesel, Sucursales);
+        ArrayAdapter <String> dueño = new ArrayAdapter<String>(this, R.layout.spinner_item_diesel, Dueños);
+        ArrayAdapter <String> ruta = new ArrayAdapter<String>(this, R.layout.spinner_item_diesel, Rutas);
+        ArrayAdapter <String> unidad = new ArrayAdapter<String>(this, R.layout.spinner_item_diesel, Unidades);
+
+        txt_sucursal.setAdapter(sucursal);
+        txt_dueño.setAdapter(dueño);
+        txt_ruta.setAdapter(ruta);
+        txt_unidad.setAdapter(unidad);
     }
 
     public void AgregarDespacho (View view){
 
         Map<String, Object> despacho = new HashMap<>();
-        despacho.put("Sucursal", txt_sucursal.getText().toString());
+        despacho.put("Sucursal", txt_sucursal.getSelectedItem().toString());
         despacho.put("Folio", Integer.parseInt(txt_folio.getText().toString()));
         despacho.put("Fecha", txt_fecha.getText().toString());
         despacho.put("Hora", txt_hora.getText().toString());
-        despacho.put("Ruta", txt_ruta.getText().toString());
-        despacho.put("Dueño", txt_dueño.getText().toString());
+        despacho.put("Ruta", txt_ruta.getSelectedItem().toString());
+        despacho.put("Dueño", txt_dueño.getSelectedItem().toString());
         despacho.put("Placas", txt_placas.getText().toString());
-        despacho.put("Unidad", txt_unidad.getText().toString());
+        despacho.put("Unidad", txt_unidad.getSelectedItem().toString());
         despacho.put("Litros", Float.parseFloat(txt_litros.getText().toString()));
         if (checkStatus.isChecked()){
             despacho.put("Estatus", "Pagado");
@@ -78,14 +97,14 @@ public class NewDispatchActivity extends AppCompatActivity {
                     }
                 });
 
-        txt_sucursal.setText(null);
+        txt_sucursal.setSelection(0);
         txt_folio.setText(null);
         txt_fecha.setText(null);
         txt_hora.setText(null);
-        txt_ruta.setText(null);
-        txt_dueño.setText(null);
+        txt_ruta.setSelection(0);
+        txt_dueño.setSelection(0);
         txt_placas.setText(null);
-        txt_unidad.setText(null);
+        txt_unidad.setSelection(0);
         txt_litros.setText(null);
 
 
